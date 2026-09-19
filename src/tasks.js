b8b528ca4d837aafa64434057db14dbd1737ad59
Thiloka Kulathunga <thilokajeewani888@gmail.com>
2026-09-19 16:20:08 +0530

    fix: update CampusEats task list

--- src/tasks.js
+++ src/tasks.js
@@ -1,7 +1,9 @@
 // CampusEats task list
-const tasks = [ 
-  "Design the menu screen", 
-  "Build the orders API", 
-  "Add user login", 
-]; 
-console.log(`CampusEats has ${tasks.length} open tasks`); 
+
+const tasks = [
+  "Design the menu screen",
+  "Build the orders API",
+  "Add user login",
+];
+
+console.log(`CampusEats has ${tasks.length} open tasks`);
